0b59a92e6cc43ef492cdbed64b5195740a595e50
SaranyaVidyananthan <s2vidyan@edu.uwaterloo.ca>
2019-09-20 21:11:24 -0400

    adding main screen

--- app/src/main/java/com/example/quizapplication/MainActivity.kt
+++ app/src/main/java/com/example/quizapplication/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
+        setContentView(R.layout.question_main)
 
         setNextQuestion()
 
